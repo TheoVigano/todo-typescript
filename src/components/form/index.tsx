81f26b63631daa37d7d3ad1a69305d3945c7f1f4
Theo Vigano <135166649+TheoVigano@users.noreply.github.com>
2025-04-09 17:56:21 -0300

    Add files via upload

---
+++ src/components/form/index.tsx
@@ -0,0 +1,16 @@
+import { PlusCircle } from "@phosphor-icons/react";
+import "./styles.css"
+
+export function Form() {
+  return (
+    <>
+      <form>
+        <input type="text" placeholder="Adicione uma nova tarefa" />
+        <button type="submit">
+          Criar
+          <PlusCircle size={20} />
+        </button>
+      </form>
+    </>
+  );
+}
